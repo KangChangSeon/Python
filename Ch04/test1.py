c20b15c581a2d9ef313e058c41cd9ccc5f694bcd
KangChangSeon <121225359+KangChangSeon@users.noreply.github.com>
2023-05-08 11:18:35 +0900

    Update test1.py

--- Ch04/test1.py
+++ Ch04/test1.py
@@ -30,3 +30,21 @@
 # print('{} Python!'.format('Hello'))
 
 # print('I like {0} and {1}'.format('Python','Java'))
+# print('{0},{0},{0}! Python'.format('Hello'))
+
+
+# name = input('당신의 이름을 입력해주세요 : ')
+# age = int(input('나이를 입력해주세요 : '))
+# job = input('직업을 입력해주세요 : ')
+# addr = input('사는 곳을 입력해 주세요 : ')
+
+# print('당신의 이름은 {}, 나이는 {}살, 직업은 {}, 사는 곳은 {}입니다.'.format(name,age,job,addr))
+
+# for i in range(2,15,2):
+#     print('{0:3d} {1:4d} {2:5d}'.format(i,i*i,i*i*i))
+
+# print('소수점 두자리로 표현한 원주율 = {0:10.2f}'.format(3.1415926))
+
+# print('{:,}'.format(1234567890))
+
+# print('위도 : {lat}, 경도 : {long}'.format(lat='35.17N', long='129.07E'))
