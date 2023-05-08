b4dfeda2578f224dace04b651f5fbb3cdbd24aec
KangChangSeon <121225359+KangChangSeon@users.noreply.github.com>
2023-05-08 11:51:34 +0900

    05-08

--- Ch05/test1.py
+++ Ch05/test1.py
@@ -1,3 +1,17 @@
 # score_list = [87,84,95,67,88,94,63]
 # print(score_list[0], score_list[3])
 
+# list1 = list()
+# list2 = []
+# #list1 과 list2 는 같은 의미의 list
+# list3 = list((1,2,3))
+# >>> list3 = [1,2,3]
+
+# list4 = list(range(1,10))
+# >>> list4 = [1,2,3,4,5,6,7,8,9]
+
+# list5 = list('ABCDEF')
+# >>> list5 = ['A','B','C','D','E','F']
+
+# list_friends = ['형태','현민','인욱','성찬','욱근']
+# print('friends = {}'.format(list_friends))
