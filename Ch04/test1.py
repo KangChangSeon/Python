3a6bb9a8dcb27524d916d95fa6ee6078d75583d8
KangChangSeon <121225359+KangChangSeon@users.noreply.github.com>
2023-05-08 10:36:09 +0900

    Update test1.py

--- Ch04/test1.py
+++ Ch04/test1.py
@@ -11,6 +11,22 @@
 # print('s1 : ',s1)
 # print('s2 : ',s2)
 
-num1,num2,num3 = map(int,input('숫자 3개 입력 : ').split())
+# num1,num2,num3 = map(int,input('숫자 3개 입력 : ').split())
 
-print('num1,num2,num3 = {},{},{}'.format(num1,num2,num3))
+# print('num1,num2,num3 = {},{},{}'.format(num1,num2,num3))
+
+# 'hello'.upper()
+# 'HELLO'
+
+# 'HELLO'.lower()
+# 'hello'
+
+# 'Guido van Rossum'.split()
+# ['Guido','van','Rossum']
+
+# 'Apple,Banana,Orange'.split(',')
+# ['Apple','Banana','Orange']
+
+# print('{} Python!'.format('Hello'))
+
+# print('I like {0} and {1}'.format('Python','Java'))
