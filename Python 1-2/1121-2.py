c4c8651e9cd06ee6e8e18a0491a9929161bb3b69
KangChangSeon <121225359+KangChangSeon@users.noreply.github.com>
2023-11-21 15:51:33 +0900

    1121

---
+++ Python 1-2/1121-2.py
@@ -0,0 +1,19 @@
+# 코드 7.1: 선택 정렬 알고리즘
+def selection_sort(A) :
+    n = len(A)
+    for i in range(n-1) :
+        least = i;
+        for j in range(i+1, n) :
+            if (A[j]<A[least]) :
+                least = j
+        A[i], A[least] = A[least], A[i]	    # 배열 항목 교환 
+        printStep(A, i + 1);	            # 중간 과정 출력용 문장 
+def printStep(arr, val) :
+    print("  Step %2d = " % val, end='')
+    print(arr)
+
+
+data = [5,3,8,4,9,1,6,2,7]
+print("Original  :", data)
+selection_sort(data)
+print("Selection :", data)
